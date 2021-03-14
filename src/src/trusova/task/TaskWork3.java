76527ab55b0478ebb87a89dbf8909ffa82ea4e99
Trusova M <rena-_-@inbox.ru>
2021-03-14 12:50:07 +0300

    среднее

--- src/src/trusova/task/TaskWork3.java
+++ src/src/trusova/task/TaskWork3.java
@@ -1,2 +1,14 @@
-package src.trusova.task;public class TaskWork3 {
+package src.trusova.task;
+
+public class TaskWork3 {
+    public static void main(String[] args){
+    int[] massive = new int[] {1, 2, 3, 4, 5};
+    int temp;
+    temp = massive[0];
+    massive[0] = massive[massive.length-1];
+    massive[massive.length-1] = temp;
+    int sred = massive.length / 2;
+    int sum = massive[0] + massive[sred];
+    System.out.println("сумма 0 и "+ (sred + 1)  +" элементов = "+ sum);
+    }
 }
