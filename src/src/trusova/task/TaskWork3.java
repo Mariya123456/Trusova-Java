133a2ecbac3cd25e7cf11eb80e797aac50ff6d0e
Trusova M <rena-_-@inbox.ru>
2021-03-16 18:26:51 +0300

    среднее

--- src/src/trusova/task/TaskWork3.java
+++ src/src/trusova/task/TaskWork3.java
@@ -9,6 +9,6 @@ public class TaskWork3 {
     massive[massive.length-1] = temp;
     int sred = massive.length / 2;
     int sum = massive[0] + massive[sred];
-    System.out.println("сумма 0 и "+ (sred + 1)  +" элементов = "+ sum);
+    System.out.println("сумма 0 и "+ (sred)  +" элементов = "+ sum);
     }
 }
