e10e052f817f1e556c2dcc2ca4cf8d4ab1ebb615
Trusova M <rena-_-@inbox.ru>
2021-03-21 23:42:26 +0300

    Work5

--- src/src/trusova/task/TaskWork5.java
+++ src/src/trusova/task/TaskWork5.java
@@ -1,2 +1,30 @@
-package src.trusova.task;public class TaskWork5 {
+package src.trusova.task;
+import java.util.Scanner;
+public class TaskWork5 {  public static void main( String[] args ) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Введите X: ");
+    int X= in.nextInt();
+    int ITOG = 0;
+    System.out.println("Вы ввели X=: " + X);
+    System.out.println("Введите Y: ");
+    int Y= in.nextInt();
+    System.out.println("Вы ввели Y=: " + Y);
+    System.out.println("Введите Символ +, -, * или / : ");
+    String SIMVOL= in.next();
+    System.out.println("Вы ввели Символ =: " + SIMVOL);
+    switch(SIMVOL){
+        case "+": ITOG = X + Y;
+            break;
+        case "-": ITOG = X - Y;
+            break;
+        case "*": ITOG = X * Y;
+            break;
+        case "/": ITOG = X / Y;
+            break;
+        default: System.out.println("Вы ввели некорректный знак");
+            break;
+    }
+    System.out.println("ИТОГ =: " + ITOG);
 }
+}
+
