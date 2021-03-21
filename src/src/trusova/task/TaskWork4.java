cde5b5abbc3d404fa9d5f3c587fbc1d1212e21ba
Trusova M <rena-_-@inbox.ru>
2021-03-21 22:50:14 +0300

    Work4

--- src/src/trusova/task/TaskWork4.java
+++ src/src/trusova/task/TaskWork4.java
@@ -1,2 +1,22 @@
-package src.trusova.task;public class TaskWork4 {
-}
+package src.trusova.task;
+import java.util.Scanner;
+public class TaskWork4 {
+    public static void main( String[] args ) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите X: ");
+        int X= in.nextInt();
+        System.out.println("Вы ввели X=: " + X);
+        System.out.println("Введите Y: ");
+        int Y= in.nextInt();
+        System.out.println("Вы ввели Y=: " + Y);
+        System.out.println("Введите Z: ");
+        int Z= in.nextInt();
+        System.out.println("Вы ввели Z=: " + Z);
+        int SREDNEE = (X + Y + Z)/3;
+        System.out.println("Среднее арифметическое =: " + SREDNEE);
+        int DELENIE = SREDNEE/2;
+        System.out.println("Деление на 2 =: " + DELENIE);
+        if (DELENIE > 3) { System.out.println("Программа выполнена корректно");
+        }
+        }
+    }
