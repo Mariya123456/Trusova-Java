642a344da1c7aeba54dd74e24c128b0f29a51f51
Trusova M <rena-_-@inbox.ru>
2021-03-23 22:40:13 +0300

    Work8

--- src/src/trusova/task/TaskWork8.java
+++ src/src/trusova/task/TaskWork8.java
@@ -1,2 +1,18 @@
-package src.trusova.task;public class TaskWork8 {
+package src.trusova.task;
+
+import java.util.Scanner;
+
+public class TaskWork8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите целое положительное число");
+        int n = in.nextInt();
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                total += i;
+            }
+        }
+        System.out.println("Сумма нечетных чисел от 1 до " + n + " = " + total);
+    }
 }
