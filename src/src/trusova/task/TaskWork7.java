94ea9f3a69bb9080b6d9f04e93fd3de311b5e671
Trusova M <rena-_-@inbox.ru>
2021-03-23 22:16:40 +0300

    Work7

--- src/src/trusova/task/TaskWork7.java
+++ src/src/trusova/task/TaskWork7.java
@@ -1,2 +1,27 @@
-package src.trusova.task;public class TaskWork7 {
+package src.trusova.task;
+
+import java.util.Scanner;
+
+public class TaskWork7 {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Введите размер массива");
+    int razmer = in.nextInt();
+    int X = 2;
+    int Y = 7;
+    int Z = 3;
+    int[] array = new int[razmer];
+    for(int i=0; i<razmer; i++){
+        System.out.println("Введите значение массива в " + i +" ячейку");
+        array[i] = in.nextInt();
+
+
+        }
+    for (int i=0; i<array.length; i++) {
+        System.out.println("Элемент массива "+ i +": " + array[i]);
+        if (array[i] == X || array[i] == Y || array[i] == Z){
+            System.out.println("Данное значение имеется в константах ");
+        }
+    }
+}
 }
